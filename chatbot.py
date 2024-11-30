650495d755ee050d99981f070a1443dfe0a5240a
Chaudhry Waleed <138867014+chauhdhryWaleed@users.noreply.github.com>
2024-11-30 22:29:29 +0500

    integrated groq

--- chatbot.py
+++ chatbot.py
@@ -2,17 +2,21 @@ import re
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import ollama
 import chromadb
+from groq import Groq
+groq_client = Groq(
+    api_key=,
+)
 
 # Initialize Chroma DB client
 client = chromadb.PersistentClient(path='database')
 my_collection = client.create_collection(
-    name="collection6",
+    name="nlp_project0",
+
     metadata={"hnsw:space": "cosine"}
 )
 
-
 class Chatbot:
-    def __init__(self, model_name="BAAI/bge-large-en-v1.5", max_chunk_size=250):
+    def __init__(self, model_name="BAAI/bge-large-en-v1.5", max_chunk_size=200):
         self.chunker = RecursiveChunker(model_name, max_chunk_size)
         self.model = "llama3.1:8b-instruct-q4_K_M"
 
@@ -22,22 +26,46 @@ class Chatbot:
 
     def query(self, user_query):
         query_embedding = self.chunker.embed_query(user_query)
-        similar_chunks = self.chunker.search_similar_chunks(query_embedding, top_k=5)
+        similar_chunks = self.chunker.search_similar_chunks(query_embedding, top_k=3)
 
         # Concatenate chunks into a single string
         result_string = "\n\n".join([chunk for chunk, _ in similar_chunks])
 
         # Use LLM to generate response
-        system_part = f"You are an expert in Question and Answering. Answer the question using the text below. If the answer isn't present, respond with 'I am sorry. I don't know the answer.'\n{result_string}"
-        prompt = (
-            f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
-            f"{system_part}\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
-            f"{user_query}\n\n<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        system_part = (
+            "You are an expert in Question and Answering. "
+            "Answer the Question by only using the text below. "
+            "If the answer isn't present in the text, respond with "
+            "'I am sorry. I don't know the answer.***'\n\n"
+            f"{result_string}***"
         )
-        conversation = [{"role": "assistant", "content": prompt}]
-        response = ollama.chat(model=self.model, messages=conversation)
 
-        return response['message']['content']
+        # prompt = (
+        #     f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+        #     f"{system_part}\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
+        #     f"{user_query}\n\n<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        # )
+        # conversation = [{"role": "assistant", "content": prompt}]
+        # response = ollama.chat(model=self.model, messages=conversation)
+
+        # return response['message']['content']
+    
+        response = groq_client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": f"{system_part}"
+            },
+            {
+                "role": "user",
+                "content": f"{user_query} "
+            }
+        ],
+        model="llama-3.1-8b-instant",
+        max_tokens=1500
+    )
+        return response.choices[0].message.content
+  
 
 class RecursiveChunker:
     def __init__(self, model_name="BAAI/bge-large-en-v1.5", max_chunk_size=150):
@@ -84,7 +112,7 @@ class RecursiveChunker:
     def embed_query(self, query):
         return self.embeddings_model.embed_documents([query])[0]
 
-    def search_similar_chunks(self, query_embedding, top_k=5):
+    def search_similar_chunks(self, query_embedding, top_k=3):
         results = self.collection.query(query_embedding, n_results=top_k)
         similar_chunks = []
         for doc_list, score_list in zip(results['documents'], results['distances']):
